305615a7263465e45009bdb1f8fc7fdf0f8e876e
iniudin <sysfdn@gmail.com>
2022-06-07 19:21:55 +0700

    tambah fitur upload foto dan lain lain

--- src/server.js
+++ src/server.js
@@ -1,33 +1,43 @@
-const Hapi = require("@hapi/hapi");
-const cityController = require("./controller/city.controller");
-const missionController = require("./controller/mission.controller");
-const placeController = require("./controller/place.controller");
-const planController = require("./controller/plan.controller");
-const userController = require("./controller/user.controller");
-const { database } = require("./models");
+const Hapi = require('@hapi/hapi');
+const cityController = require('./controller/city.controller');
+const missionController = require('./controller/mission.controller');
+const placeController = require('./controller/place.controller');
+const planController = require('./controller/plan.controller');
+const userController = require('./controller/user.controller');
+const { database } = require('./models');
 
 (async () => {
-  const server = Hapi.server({
+  const server = new Hapi.Server({
+    host: 'localhost',
     port: 3000,
-    host: "localhost",
-  });
+});
 
   try {
     await database.sync({ force: true });
-    console.log("Database Initialized");
+    console.log('Database Initialized');
   } catch (error) {
     console.log(error);
   }
 
-  await server.register(require("@hapi/basic"));
+  await server.register(require('@hapi/basic'));
+  await server.register(require('@hapi/inert'));
 
   server.route(
     [
       {
-        method: "GET",
-        path: "/",
-        handler: async () => ({ message: "Server sedang berjalan" }),
+        method: 'GET',
+        path: '/',
+        handler: async () => ({ message: 'Server sedang berjalan' }),
       },
+      {
+        method: 'GET',
+        path: '/images/{file*}',
+        handler: {
+          directory: {
+            path: 'public'
+          }
+        }
+      }
     ].concat(
       userController,
       cityController,
@@ -38,5 +48,5 @@ const { database } = require("./models");
   );
 
   await server.start();
-  console.log("Server running on %s", server.info.uri);
+  console.log('Server running on %s', server.info.uri);
 })();
